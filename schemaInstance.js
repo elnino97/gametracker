88f5a462909d1e3d8c1cec30abd00b0ba3362b55
Aidas <aidas19970711@gmail.com>
2021-08-01 23:08:52 +0300

    Cut unnecessary code

--- schemaInstance.js
+++ schemaInstance.js
@@ -1,11 +1,6 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const User = require('./models/user');
 const Activity = require('./models/activity');
-const Game = require('./models/gameinfo');
 
-module.exports.newActivity = async (action, user, game) => {
-    const activity = await new Activity({ action, user, game})
+module.exports.newActivity = async (action, user, game, review) => {
+    const activity = await new Activity({ action, user, game, review})
     await activity.save()
 }
