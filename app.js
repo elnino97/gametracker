1f047f6bcd1699ab1e60464168b8206e17987b2c
Aidas <aidas19970711@gmail.com>
2021-08-19 21:35:08 +0300

    Add public directory

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.static(path.join(__dirname, 'public')))
 
