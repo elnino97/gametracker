151219a22ec005b9ebc9168901d57850daec6478
Aidas <aidas19970711@gmail.com>
2021-08-25 16:46:37 +0300

    Add game img

--- schemaInstance.js
+++ schemaInstance.js
@@ -1,6 +1,6 @@
 const Activity = require('./models/activity');
 
-module.exports.newActivity = async (action, user, game, review) => {
-    const activity = await new Activity({ action, user, game, review})
+module.exports.newActivity = async (action, user, game, review, image) => {
+    const activity = await new Activity({ action, user, game, review, image})
     await activity.save()
 }
