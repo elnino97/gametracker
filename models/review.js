487ed62b31833e4661b6db205d30521cbfcf9cda
Aidas <aidas19970711@gmail.com>
2021-09-10 11:52:16 +0300

    Add virtuals for smaller images

--- models/review.js
+++ models/review.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('smallScreenshot').get(function () {
+    return this.url.replace('/upload', '/upload/w_135');
+});
+ImageSchema.virtual('mediumScreenshot').get(function () {
+    return this.url.replace('/upload', '/upload/w_280');
+});
+
 const reviewSchema = new Schema({
     title: String,
     rating: Number,
     body: String,
+    screenshots: [ImageSchema],
     recommend: Boolean,
     gameId: Number,
     game: {
