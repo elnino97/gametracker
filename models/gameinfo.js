ce3210269a300e3a2a72c120401663de93445c37
Aidas <aidas19970711@gmail.com>
2021-09-10 11:52:44 +0300

    Add virtual for screenshots

--- models/gameinfo.js
+++ models/gameinfo.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('mediumScreenshot').get(function () {
+    return this.url.replace('/upload', '/upload/w_280');
+});
+
 const gameSchema = new Schema({
     id: Number,
     name: String,
     background_image: String,
     released: String,
+    screenshots: [ImageSchema],
     favoritedTimes: {
         type: Number,
         default: 0
