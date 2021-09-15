ef660960ac272cf9b3ef50ecf25274ebe76277ce
Aidas <aidas19970711@gmail.com>
2021-09-15 18:13:51 +0300

    Switch from local db to Mongo

--- app.js
+++ app.js
@@ -24,7 +24,14 @@ const app = express();
 const ExpressError = require('./utils/ExpressError');
 const User = require('./models/user')
 
-mongoose.connect('mongodb://localhost:27017/playable', {useNewUrlParser: true, useUnifiedTopology: true});
+const dbUrl = process.env.DB_URL ||'mongodb://localhost:27017/playable';
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
 
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
